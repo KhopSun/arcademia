8c1143b4d04a3668a9b504dac60ca171e2f32a7b
KhopSun <khopher2548@gmail.com>
2025-03-29 17:37:23 +0700

    Clerk

--- arcademia-web/app/layout.tsx
+++ arcademia-web/app/layout.tsx
@@ -3,6 +3,16 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import BottomNavBar from "./components/BottomNavBar";
 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import './globals.css'
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,13 +34,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-        <BottomNavBar />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+         <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+            <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+           </SignedIn>
+          </header>
+          {children}
+        </body>
+        <BottomNavBar/>
+      </html>
+    </ClerkProvider>
   );
 }
