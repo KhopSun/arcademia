7e0e41cc10c477c2b6b53c4ae4404b5d5d2f7bce
Tatchai-Tee <themeturbo8@gmail.com>
2025-03-29 18:55:37 +0700

    added progress bar

--- arcademia-web/app/(main)/profile/page.tsx
+++ arcademia-web/app/(main)/profile/page.tsx
@@ -1,19 +1,32 @@
 import React from "react";
+import profileBg from "../../assets/background/profilebg.png";
+
+const level = 1;
+const currentXp = 500;
+const neededXp = 1000;
 
 const ProfilePage: React.FC = () => {
   return (
     <div
-      className="w-full min-h-screen bg-cover bg-center relative p-4"
+      className="w-full min-h-screen bg-cover bg-center relative p-6"
       style={{ backgroundImage: `url('/wood.png')` }}
     >
+      <div
+        className="flex items-center justify-center w-full p-6 bg-cover bg-center h-[390px]"
+        style={{ backgroundImage: `url(${profileBg.src})` }}
+      >
+        <div className="flex w-full justify-center items-center gap-4">
+          <span>Lvl.{level}</span>
+          <progress
+            className="nes-progress is-success"
+            style={{height: '10px'}}
+            value={currentXp}
+            max={neededXp}
+          ></progress>
+        </div>
+      </div>
       <h1 className="text-2xl font-bold">Profile Page</h1>
       <p>Welcome to the profile page!</p>
-
-      <div className="nes-container is-dark with-title">
-        <p className="title">Container.is-dark</p>
-        <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
-      </div>
-
     </div>
   );
 };
