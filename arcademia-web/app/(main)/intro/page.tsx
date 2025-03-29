a6cec4e0667f906d92c56664964b4e34693d454f
KTAP8 <touch.apinankul@gmail.com>
2025-03-30 00:32:18 +0700

    change loading screen color

--- arcademia-web/app/(main)/intro/page.tsx
+++ arcademia-web/app/(main)/intro/page.tsx
@@ -47,8 +47,8 @@ export default function Home() {
     //   <br />
     //   {/* TODO: Render your next game component here */}
     // </div>
-    <div className="flex items-center justify-center h-screen bg-white text-black">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500"></div>
+    <div className="flex items-center justify-center h-screen bg-[#EBDEC0] text-black">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-[#47423F]"></div>
       <span className="ml-4 text-xl">Loading next stage...</span>
     </div>
   );
