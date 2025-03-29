e7ce11c059c64fe699bab30027b3657e3f1613f4
Tatchai-Tee <themeturbo8@gmail.com>
2025-03-29 21:28:49 +0700

    Finished stats

--- arcademia-web/app/(main)/profile/page.tsx
+++ arcademia-web/app/(main)/profile/page.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import profileBg from "@/public/assets/background/profilebg.png";
 import woodBg from "@/public/assets/background/wood.png";
+import codeIcon from "@/public/assets/skillTreeElements/coding_element.png";
+import mathIcon from "@/public/assets/skillTreeElements/math_element.png";
+import scienceIcon from "@/public/assets/skillTreeElements/science_element.png";
+import languageIcon from "@/public/assets/skillTreeElements/english_element.png";
+import maleChar from '@/public/assets/main_char/steve.png';
+import Image from "next/image";
 
 const level = 1;
 const currentXp = 500;
 const neededXp = 1000;
+const name = "Tachios";
 
 const ProfilePage: React.FC = () => {
   return (
@@ -13,24 +20,94 @@ const ProfilePage: React.FC = () => {
       style={{ backgroundImage: `url(${woodBg.src})` }}
     >
       <div
-        className="flex items-center justify-center w-full p-9 bg-center bg-cover h-[390px]"
+        className="flex-col my-6 gap-4 flex items-center justify-center w-full p-9 bg-center bg-cover h-[390px]"
         style={{ backgroundImage: `url(${profileBg.src})` }}
       >
-        <div className="flex w-full justify-center items-center gap-4">
+        <Image alt='male' src={maleChar} width={200} height={200}></Image>
+        <div className="nes-container !p-0 justify-center items-center is-rounded">
+          <span className="text-2xl">{name}</span>
+        </div>
+        <div className="flex w-full justify-center items-center gap-4" >
           <span>Lvl.{level}</span>
           <progress
             className="nes-progress is-success"
-            style={{ height: "15px" }}
+            style={{ height: "15px"}}
             value={currentXp}
             max={neededXp}
           ></progress>
         </div>
-        <div className="nes-container is-rounded">
-          <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
-        </div>
       </div>
-      <h1 className="text-2xl font-bold">Profile Page</h1>
-      <p>Welcome to the profile page!</p>
+      <div className="!w-full">
+        <table className="!w-full">
+          <tbody>
+            <tr>
+              <td className="w-[70px]">
+                <Image
+                  alt="science"
+                  src={scienceIcon}
+                  width={70}
+                  height={70}
+                ></Image>
+              </td>
+              <td>
+                <progress
+                  className="nes-progress is-error !w-full !h-[15px]"
+                  value="70"
+                  max="100"
+                ></progress>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Image alt="math" src={mathIcon} width={70} height={70}></Image>
+              </td>
+              <td>
+                <progress
+                  className="nes-progress is-primary !w-full !h-[15px]"
+                  value="30"
+                  max="100"
+                ></progress>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div className="flex items-center h-[70px] w-[70px] justify-center">
+                  <Image
+                    alt="coding"
+                    src={codeIcon}
+                    width={60}
+                    height={60}
+                  ></Image>
+                </div>
+              </td>
+              <td>
+                <progress
+                  className="nes-progress is-warning !w-full !h-[15px]"
+                  value="50"
+                  max="100"
+                ></progress>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Image
+                  alt="language"
+                  src={languageIcon}
+                  width={70}
+                  height={70}
+                ></Image>
+              </td>
+              <td>
+                <progress
+                  className="nes-progress is-success !w-full !h-[15px]"
+                  value="90"
+                  max="100"
+                ></progress>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
