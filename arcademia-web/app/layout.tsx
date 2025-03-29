c471c3117d68f1fe0b4c4da404e729dbb65396f3
Tatchai-Tee <themeturbo8@gmail.com>
2025-03-29 19:12:45 +0700

    Set pressstart2p as default font

--- arcademia-web/app/layout.tsx
+++ arcademia-web/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${pressStart2P.className} ${geistMono.variable} antialiased`}
         >
          <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
